c0346accf98323045436c75cd232e35016abd6d4
楊凱丞 <mmilsh2648@gmail.com>
2023-03-29 11:01:53 +0800

    commitframe開始，兩個按鈕

--- grid/index.py
+++ grid/index.py
@@ -6,28 +6,28 @@ import tkinter as tk
 from tkinter import ttk
 
 class Window(tk.Tk):
-    def __init__(self, **kwargs) -> None:
+    def __init__(self,**kwargs):
         super().__init__(**kwargs)
         ttkStyle = ttk.Style()
         ttkStyle.theme_use('default')
-        ttkStyle.configure('red.TFrame', background='#ff0000')
+        ttkStyle.configure('red.TFrame',background='#ff0000')
         ttkStyle.configure('white.TFrame',background='#ffffff')
         ttkStyle.configure('yellow.TFrame',background='yellow')
-        ttkStyle.configure('white.TLabel', background='#ffffff')
-        ttkStyle.configure('gridLabel.TLabel', font=('Helvetica', 16),foreground='#666666')
-        ttkStyle.configure('gridEntry.TEntry', font=('Helvetica', 16))
+        ttkStyle.configure('white.TLabel',background='#ffffff')
+        ttkStyle.configure('gridLabel.TLabel',font=('Helvetica', 16),foreground='#666666')
+        ttkStyle.configure('gridEntry.TEntry',font=('Helvetica', 16))
+        
+        mainFrame = ttk.Frame(self)        
+        mainFrame.pack(expand=True,fill=tk.BOTH,padx=30,pady=30)
+        
 
+        topFrame = ttk.Frame(mainFrame,height=100)
+        topFrame.pack(fill=tk.X)
 
-        mainFrame = ttk.Frame(self)
-        mainFrame.pack(expand = True, fill =tk.BOTH,padx=30,pady=30)
-
-        topFrame = ttk.Frame(mainFrame, heigh = 100)
-        topFrame.pack(expand = True, fill = tk.X)
-
-        ttk.Label(topFrame, text="BMI試算表", font=('Helvetica', '20')).pack(pady=20)
+        ttk.Label(topFrame,text="BMI試算",font=('Helvetica', '20')).pack(pady=20)
 
         bottomFrame = ttk.Frame(mainFrame)
-        bottomFrame.pack(expand=True, fill=tk.BOTH)
+        bottomFrame.pack(expand=True,fill=tk.BOTH)
         bottomFrame.columnconfigure(0,weight=3,pad=20)
         bottomFrame.columnconfigure(1,weight=5,pad=20)
         bottomFrame.rowconfigure(0, weight=1,pad=20)
@@ -37,10 +37,9 @@ class Window(tk.Tk):
         bottomFrame.rowconfigure(6, weight=1,pad=20)
         
 
-        ttk.Label(bottomFrame, text="姓名:", style='gridLabel.TLabel').grid(column=0, row=0, sticky=tk.E)
+        ttk.Label(bottomFrame,text="姓名:",style='gridLabel.TLabel').grid(column=0,row=0,sticky=tk.E)
         nameEntry = ttk.Entry(bottomFrame,style='gridEntry.TEntry')
         nameEntry.grid(column=1,row=0,sticky=tk.W,padx=10)
-    
 
         ttk.Label(bottomFrame,text="出生年月日:",style='gridLabel.TLabel').grid(column=0,row=1,sticky=tk.E)
         ttk.Label(bottomFrame,text="(2000/03/01)",style='gridLabel.TLabel').grid(column=0,row=2,sticky=tk.E)
@@ -55,13 +54,25 @@ class Window(tk.Tk):
         ttk.Label(bottomFrame,text="體重(kg):",style='gridLabel.TLabel').grid(column=0,row=4,sticky=tk.E)
         weightEntry = ttk.Entry(bottomFrame,style='gridEntry.TEntry')
         weightEntry.grid(column=1,row=4,sticky=tk.W,padx=10)
-        
+
         messageText = tk.Text(bottomFrame,height=5,width=35,state=tk.DISABLED,takefocus=0,bd=0)
         messageText.grid(column=0,row=5,sticky=tk.N+tk.S,columnspan=2)
 
-        commitBtn = ttk.Button(bottomFrame,text="計算")
+        #------commitFrame開始-----------
+        #有2個按鈕
+
+        commitFrame = ttk.Frame(bottomFrame)
+        commitFrame.grid(column=0,row=6,columnspan=2)
+
+        commitBtn = ttk.Button(commitFrame,text="計算")
+        commitBtn.grid(column=0,row=0,sticky=tk.W)
+
+        clearBtn = ttk.Button(commitFrame,text="清除")
+        clearBtn.grid(column=1,row=0,sticky=tk.E)
+        #------commitFrame結束-----------
+
+
         
-        commitBtn.grid(column=1,row=6,sticky=tk.W)
 
 
 
