2eaa412d632900dacb09712513198392dfc66b7a
楊凱丞 <mmilsh2648@gmail.com>
2023-04-07 15:33:04 +0800

    modify

--- youbike/main.py
+++ youbike/main.py
@@ -1,7 +1,23 @@
 import datasource
+import tkinter as tk
+from tkinter import ttk
+
+class Window(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        topFrame = ttk.LabelFrame(self,text='台北市行政區')
+        length = len(datasource.sarea_list)
+        for i in range(length):
+            cols = i % 3
+            rows = i // 3
+            ttk.Radiobutton(topFrame,text=datasource.sarea_list[i]).grid(column=cols,row=rows)
+        topFrame.pack()
 
 def main():
-    print(datasource.sarea_list)
+    # print(datasource.sarea_list)
+    window = Window()
+    window.title('台北市youbike2.0資訊')
+    window.mainloop()
     
 
 if __name__ == '__main__':
