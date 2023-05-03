862de9693c985364bb8aa800bb2b4db4e7f95934
Yang Kai Cheng <mmilsh2648@gmail.com>
2023-05-03 15:06:37 +0800

    modify

--- Flask/0503/app.py
+++ Flask/0503/app.py
@@ -8,3 +8,8 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.jinja.html")
+
+#點進網址後，新增/learning
+@app.route("/learning/")
+def learning():
+    return render_template("learning.jinja.html")
