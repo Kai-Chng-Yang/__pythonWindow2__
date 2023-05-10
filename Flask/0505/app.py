d39568bd04d38d928311654e69f35fc9494df807
YKC <mmilsh2648@gmail.com>
2023-05-10 11:29:01 +0800

    modify

--- Flask/0505/app.py
+++ Flask/0505/app.py
@@ -1,20 +1,13 @@
-
-
 #執行 flask --app app run --debug
 
 from flask import Flask,render_template
-import pandas_datareader.data as pdr
-import yfinance as yf
-
+import datasource
 
 app = Flask(__name__)
 @app.route("/")
 def index():
-    yf.pdr_override()
-    tw_2303 = pdr.get_data_yahoo('2303.TW')
-    series_2303 = tw_2303['Adj Close']
-    return render_template("index.jinja.html",stock2303=series_2303)
-
+    stock_data = datasource.get_stock_data(stockid=2303)
+    return  render_template("index.jinja.html",stockid=2303,stock_data=stock_data)
 
 @app.route("/features/")
 def features():
---
+++ Flask/0505/datasource.py
@@ -0,0 +1,17 @@
+'''
+這是專門取得台灣股市資料
+'''
+import pandas_datareader.data as pdr
+import yfinance as yf
+
+def get_stock_data(stockid):
+    '''
+    @parma stockid是股票代碼
+    '''
+    yf.pdr_override()
+    stockid_str = f'{stockid}.TW'    
+    stock_dataFrame = pdr.get_data_yahoo(stockid_str)
+    stock_dataFrame1 = stock_dataFrame.reset_index()
+    stock_dataFrame1['Date']= stock_dataFrame1['Date'].map(lambda x:f'{x.year}-{x.month}-{x.day}')
+    stock_list = stock_dataFrame1.to_numpy().tolist()
+    return stock_list
