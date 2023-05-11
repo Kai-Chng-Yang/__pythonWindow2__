195da451692b13046658c426a43af34123345e5d
YKC <mmilsh2648@gmail.com>
2023-05-11 09:47:33 +0800

    複製貼上0505

---
+++ Flask/0511/app.py
@@ -0,0 +1,24 @@
+#執行 flask --app app run --debug
+
+from flask import Flask,render_template
+import datasource
+
+app = Flask(__name__)
+@app.route("/")
+def index():
+    stock_data = datasource.get_stock_data(stockid=2303)    
+    return render_template("index.jinja.html",data=stock_data)
+
+@app.route("/features/")
+def features():
+    
+    return render_template("features.jinja.html")
+
+@app.route("/priceing/")
+def priceing():
+    
+    return render_template("priceing.jinja.html")
+
+@app.route("/about/")
+def about():
+    return render_template("about.jinja.html")
