425c8558111da7eacec8b74c6c9562b75aa84312
YKC <mmilsh2648@gmail.com>
2023-05-11 10:47:24 +0800

    form表單

--- Flask/0511/app.py
+++ Flask/0511/app.py
@@ -1,13 +1,13 @@
-#執行 flask --app app run --debug
-
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 import datasource
+
 
 app = Flask(__name__)
 @app.route("/")
 def index():
-    stock_data = datasource.get_stock_data(stockid=2303)    
+    stock_data = datasource.get_stock_data(stockid=2330)    
     return render_template("index.jinja.html",data=stock_data)
+
 
 @app.route("/features/")
 def features():
@@ -22,3 +22,17 @@ def priceing():
 @app.route("/about/")
 def about():
     return render_template("about.jinja.html")
+
+@app.route("/form/",methods=['GET', 'POST'])
+def form():
+    if request.method == 'POST':
+        print(f"{request.form['username']}，這是post，您好")
+        return render_template("form.jinja.html")
+    elif request.method == 'GET' :
+        return render_template("form.jinja.html")
+
+
+
+
+
+    
