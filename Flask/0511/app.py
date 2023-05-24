f35def9d04992f0de9475107f16f47fa2bb67429
YKC <mmilsh2648@gmail.com>
2023-05-24 14:42:10 +0800

    venv重裝, code複製貼上測試

--- Flask/0511/app.py
+++ Flask/0511/app.py
@@ -1,5 +1,8 @@
 from flask import Flask,render_template,request
 import datasource
+import plotly.express as px
+import json
+import plotly
 
 
 app = Flask(__name__)
@@ -10,18 +13,26 @@ def index():
         stock_name = request.form['stock_name']  #1203-味王
         stock_id = stock_name[:4]
         year =  request.form['year']
-        stock_data = datasource.get_stock_data(stockid=stock_id,year=year)        
-        return render_template("form.jinja.html",rows=rows,stock_name=stock_name,year=year,data=stock_data)
-    
-    
+        stock_dataFrame = datasource.get_stock_data(stockid=stock_id,year=year)  
+        stock_data = stock_dataFrame.to_numpy().tolist()
+        #plotly express
+        fig = px.line(stock_dataFrame,x="Date",y="Adj Close",title=f'{stock_name}_{year}')
+        graphJSON = json.dumps(fig,cls=plotly.utils.PlotlyJSONEncoder)
+        return render_template("form.jinja.html",rows=rows,stock_name=stock_name,year=year,data=stock_data,graphJSON=graphJSON)  
     return render_template("form.jinja.html",rows=rows)
 
-@app.route("/priceing/")
+
+@app.route("/features")
+def features():
+    
+    return render_template("features.jinja.html")
+
+@app.route("/priceing")
 def priceing():
     
     return render_template("priceing.jinja.html")
 
-@app.route("/about/")
+@app.route("/about")
 def about():
     return render_template("about.jinja.html")
 
