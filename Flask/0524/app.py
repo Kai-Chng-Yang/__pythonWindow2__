48e87243274f2356b0586b85f9b54384485f4517
YKC <mmilsh2648@gmail.com>
2023-05-29 09:21:08 +0800

    modify

--- Flask/0524/app.py
+++ Flask/0524/app.py
@@ -15,16 +15,17 @@ def index():
 
 @app.route("/index1")
 def index1():
-    df = px.data.tips()
-    fig = px.scatter(df, x="total_bill", y="tip", trendline="ols")
+    df = px.data.gapminder().query("continent == 'Oceania'")
+    fig = px.line(df, x='year', y='lifeExp', color='country')
     graphJSON = json.dumps(fig,cls=plotly.utils.PlotlyJSONEncoder)
 
     return render_template('index1.jinja.html',graphJSON=graphJSON)
 
 @app.route("/index2")
 def index2():
-    df = px.data.tips()
-    fig = px.scatter(df, x="total_bill", y="tip", trendline="ols")
+    df = px.data.gapminder().query("country in ['Canada', 'Botswana']")
+    fig = px.line(df, x="lifeExp", y="gdpPercap", color="country", text="year")
+    fig.update_traces(textposition="bottom right")
     graphJSON = json.dumps(fig,cls=plotly.utils.PlotlyJSONEncoder)
 
     return render_template('index2.jinja.html',graphJSON=graphJSON)
